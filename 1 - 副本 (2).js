587413303ecbf81fcdc920db3009e6b9f9b03b22
何科盛 <31618447+sailor1024@users.noreply.github.com>
2017-12-31 00:51:03 +0800

    Update 1 - 副本 (2).js

--- 1 - 副本 (2).js
+++ 1 - 副本 (2).js
@@ -1,265 +1,199 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.edu.gdmec.android.boxuegu.R;
-import cn.edu.gdmec.android.boxuegu.view.CourseView;
-import cn.edu.gdmec.android.boxuegu.view.ExercisesView;
-import cn.edu.gdmec.android.boxuegu.view.MyInfoView;
+import cn.edu.gdmec.android.boxuegu.adapter.VideoListAdapter;
+import cn.edu.gdmec.android.boxuegu.bean.VideoBean;
+import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
+import cn.edu.gdmec.android.boxuegu.utils.DBUtils;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    //    视图
-    private CourseView  mCourseView;
-    private ExercisesView mExercisesView;
-    private MyInfoView mMyInfoView;
-    private FrameLayout mBodyLayout;
-    private LinearLayout mBottomLayout;
-
-
-    private View mCourseBtn;
-    private View mExercisesBtn;
-    private View mMyInfoBtn;
-
-    private TextView tv_course;
-    private TextView tv_exercises;
-    private TextView tv_myInfo;
-
-    private ImageView iv_course;
-    private ImageView iv_exercises;
-    private ImageView iv_myInfo;
-
-    private TextView tv_back;
-    private TextView tv_main_title;
-
-    private RelativeLayout rl_title_bar;
+public class VideoListActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView tv_intro,tv_video,tv_chapter_intro;
+    private ListView lv_video_list;
+    private ScrollView sv_chapter_intro;
+    private VideoListAdapter adapter;
+    private List<VideoBean> videoList;
+    private int chapterId;
+    private String intro;
+    private DBUtils db;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_video_list);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        chapterId = getIntent().getIntExtra("id",0);
+        intro = getIntent().getStringExtra("intro");
+        db = DBUtils.getInstance(VideoListActivity.this);
+        initData();
         init();
-        initBottomBar();
-        setListener();
-        setInitStatus();
-
     }
-
-
 
     private void init() {
-        tv_back = (TextView) findViewById(R.id.tv_back);
-        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
-        tv_main_title.setText("博学谷课程");
-        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
-        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
-        tv_back.setVisibility(View.GONE);
-        initBodyLayout();
-    }
-
-
-    private void initBottomBar() {
-        mBottomLayout = (LinearLayout) findViewById(R.id.main_bottom_bar);
-        mCourseBtn = findViewById(R.id.bottom_bar_course_btn);
-        mExercisesBtn = findViewById(R.id.bottom_bar_exercises_btn);
-        mMyInfoBtn = findViewById(R.id.bottom_bar_myinfo_btn);
-
-        tv_course = (TextView) findViewById(R.id.bottom_bar_text_course);
-        tv_exercises = (TextView) findViewById(R.id.bottom_bar_text_exercises);
-        tv_myInfo = (TextView) findViewById(R.id.bottom_bar_text_myinfo);
-
-        iv_course = (ImageView) findViewById(R.id.bottom_bar_image_course);
-        iv_exercises = (ImageView) findViewById(R.id.bottom_bar_image_exercises);
-        iv_myInfo = (ImageView) findViewById(R.id.bottom_bar_image_myinfo);
-    }
-    private void initBodyLayout() {
-        mBodyLayout = (FrameLayout) findViewById(R.id.main_body);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bottom_bar_course_btn:
-                clearBottomImageState();
-                selectDisplayView(0);
-                break;
-            case R.id.bottom_bar_exercises_btn:
-                clearBottomImageState();
-                selectDisplayView(1);
-                break;
-            case R.id.bottom_bar_myinfo_btn:
-                clearBottomImageState();
-                selectDisplayView(2);
-                    if (mMyInfoView != null){
-                        mMyInfoView.setLoginParams(readLoginStatus());
-
+        tv_intro = (TextView) findViewById(R.id.tv_intro);
+        tv_video = (TextView) findViewById(R.id.tv_video);
+        lv_video_list = (ListView) findViewById(R.id.lv_video_list);
+        tv_chapter_intro = (TextView) findViewById(R.id.tv_chapter_intro);
+        sv_chapter_intro = (ScrollView) findViewById(R.id.sv_chapter_intro);
+        adapter = new VideoListAdapter(this, new VideoListAdapter.OnSelectListener() {
+            @Override
+            public void onSelect(int position, ImageView iv) {
+                adapter.setSelectedPosition(position);
+                VideoBean bean = videoList.get(position);
+                String videoPath = bean.videoPath;
+                adapter.notifyDataSetChanged();
+                if (TextUtils.isEmpty(videoPath)){
+                    Toast.makeText(VideoListActivity.this,
+                            "本地没有此视频，暂无法播放",Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    if (readLoginStatus()){
+                        String userName = AnalysisUtils.readLoginUserName(VideoListActivity.this);
+                        db.saveVideoPlayList(videoList.get(position),userName);
                     }
-                break;
-            default:
-                break;
-
-        }
-    }
-
-
-
-    private void setListener() {
-        for (int i = 0; i< mBottomLayout.getChildCount(); i++){
-            mBottomLayout.getChildAt(i).setOnClickListener(this);
-        }
-    }
-    private void clearBottomImageState() {
-        tv_course.setTextColor(Color.parseColor("#666666"));
-        tv_exercises.setTextColor(Color.parseColor("#666666"));
-        tv_myInfo.setTextColor(Color.parseColor("#666666"));
-        iv_course.setImageResource(R.drawable.main_course_icon);
-        iv_exercises.setImageResource(R.drawable.main_exercises_icon);
-        iv_myInfo.setImageResource(R.drawable.main_my_icon);
-        for (int i = 0; i<mBottomLayout.getChildCount();i++){
-            mBottomLayout.getChildAt(i).setSelected(false);
-        }
-    }
-    private void setSelectedStatus(int index) {
-        switch (index){
-            case 0:
-                mCourseBtn.setSelected(true);
-                iv_course.setImageResource(R.drawable.main_course_icon_selected);
-                tv_course.setTextColor(Color.parseColor("#0097F7"));
-                rl_title_bar.setVisibility(View.VISIBLE);
-                tv_main_title.setText("博学谷课程");
-                break;
-            case 1:
-                mExercisesBtn.setSelected(true);
-                iv_exercises
-                        .setImageResource(R.drawable.main_course_icon_selected);
-                tv_exercises.setTextColor(Color.parseColor("#0097F7"));
-                rl_title_bar.setVisibility(View.VISIBLE);
-                tv_main_title.setText("博学谷习题");
-                break;
-            case 2:
-                mMyInfoBtn.setSelected(true);
-                iv_myInfo
-                        .setImageResource(R.drawable.main_course_icon_selected);
-                tv_myInfo.setTextColor(Color.parseColor("#0097F7"));
-                rl_title_bar.setVisibility(View.GONE);
-        }
-    }
-    private void removeAllView(){
-        for (int i = 0; i<mBodyLayout.getChildCount();i++){
-            mBodyLayout.getChildAt(i).setVisibility(View.GONE);
-        }
-    }
-    private void setInitStatus(){
-        clearBottomImageState();
-        setSelectedStatus(0);
-        createView(0);
-
-    }
-
-    private void selectDisplayView(int index) {
-        removeAllView();
-        createView(index);
-        setSelectedStatus(index);
-    }
-
-    private void createView(int viewIndex) {
-        switch (viewIndex){
-            case 0:
-                //课程界面
-                if (mCourseView==null){
-                    mCourseView=new CourseView(this);
-                    mBodyLayout.addView(mCourseView.getView());
-                }else{
-                    mCourseView.getView();
+                    Intent intent  = new Intent(VideoListActivity.this,VideoPlayActivity.class);
+                    intent.putExtra("videoPath",videoPath);
+                    intent.putExtra("position",position);
+                    startActivityForResult(intent,1);
                 }
-                mCourseView.showView();
-                break;
-            case 1:
-                if (mExercisesView == null) {
-                    mExercisesView = new ExercisesView(this);
-                    mBodyLayout.addView(mExercisesView.getView());
-                }else {
-                    mExercisesView.getView();
-                }
-                mExercisesView.showView();
-                break;
-            case 2:
-                if (mMyInfoView==null){
-                    mMyInfoView=new MyInfoView(this);
-                    mBodyLayout.addView(mMyInfoView.getView());
-                }else{
-                    mMyInfoView.getView();
-                }
-                mMyInfoView.showView();
-                break;
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        if (data!=null){
-            boolean isLogin=data.getBooleanExtra("isLogin",false);
-            if (isLogin){
-                clearBottomImageState();
-                selectDisplayView(0);
             }
-            if (mMyInfoView != null){
-                mMyInfoView.setLoginParams(isLogin);
-            }
-        }
-    }
-    protected long exitTime;
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-        if (keyCode == KeyEvent.KEYCODE_BACK
-                && event.getAction() == KeyEvent.ACTION_DOWN  ){
-            if ((System.currentTimeMillis()- exitTime)>2000) {
-                Toast.makeText(MainActivity.this,"再按一次退出博学谷",Toast.LENGTH_SHORT).show();
-                exitTime = System.currentTimeMillis();
-            }else {
-                MainActivity.this.finish();
-                if (readLoginStatus()){
-                    clearLoginStatus();
+        });
+        lv_video_list.setAdapter(adapter);
+        tv_intro.setOnClickListener(this);
+        tv_video.setOnClickListener(this);
+        adapter.setData(videoList);
+        tv_chapter_intro.setText(intro);
+        tv_intro.setBackgroundColor(Color.parseColor("#30B4FF"));
+        tv_video.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        tv_intro.setTextColor(Color.parseColor("#FFFFFF"));
+        tv_video.setTextColor(Color.parseColor("#000000"));
 
+    }
+
+
+    private void initData() {
+        JSONArray jsonArray;
+        InputStream is = null;
+        try {
+            is = getResources().getAssets().open("data.json");
+            jsonArray = new JSONArray(read(is));
+            videoList = new ArrayList<VideoBean>();
+            for (int i = 0;i < jsonArray.length();i++){
+                VideoBean bean = new VideoBean();
+                JSONObject jsonObj = jsonArray.getJSONObject(i);
+                if (jsonObj.getInt("chapterId")==chapterId){
+                    bean.chapterId = jsonObj.getInt("chapterId");
+                    bean.videoId=Integer.parseInt(jsonObj.getString("videoId"));
+                    bean.title = jsonObj.getString("title");
+                    bean.secondTitle = jsonObj.getString("secondTitle");
+                    bean.videoPath = jsonObj.getString("videoPath");
+                    videoList.add(bean);
                 }
-                System.exit(0);
+                bean=null;
             }
-            return  true;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return super.onKeyDown(keyCode, event);
     }
 
-    private boolean readLoginStatus() {
-        SharedPreferences sp = getSharedPreferences("LoginInfo",
+    private String read(InputStream in){
+        BufferedReader reader = null;
+        StringBuilder sb = null;
+        String line = null;
+
+        try {
+            sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(in));
+            while ((line = reader.readLine())!=null){
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }finally {
+            if (in!=null)
+                try {
+                    in.close();
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        return sb.toString();
+    }
+
+    private boolean readLoginStatus(){
+        SharedPreferences sp = getSharedPreferences("loginInfo",
                 Context.MODE_PRIVATE);
         boolean isLogin = sp.getBoolean("isLogin",false);
         return isLogin;
     }
-    private void clearLoginStatus(){
-        SharedPreferences sp = getSharedPreferences("LoginInfo",
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("isLogin",false);
-        editor.putString("loginUserName","");
-        editor.commit();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data!=null){
+            int position = data.getIntExtra("position",0);
+            adapter.setSelectedPosition(position);
+            lv_video_list.setVisibility(View.VISIBLE);
+            sv_chapter_intro.setVisibility(View.GONE);
+            tv_intro.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            tv_video.setBackgroundColor(Color.parseColor("#30B4FF"));
+            tv_intro.setTextColor(Color.parseColor("#000000"));
+            tv_video.setTextColor(Color.parseColor("#FFFFFF"));
+
+        }
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_intro:
+                lv_video_list.setVisibility(View.GONE);
+                sv_chapter_intro.setVisibility(View.VISIBLE);
+                tv_intro.setBackgroundColor(Color.parseColor("#30B4FF"));
+                tv_video.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                tv_intro.setTextColor(Color.parseColor("#FFFFFF"));
+                tv_video.setTextColor(Color.parseColor("#000000"));
+                break;
+            case R.id.tv_video:
+                lv_video_list.setVisibility(View.VISIBLE);
+                sv_chapter_intro.setVisibility(View.GONE);
+                tv_intro.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                tv_video.setBackgroundColor(Color.parseColor("#30B4FF"));
+                tv_intro.setTextColor(Color.parseColor("#000000"));
+                tv_video.setTextColor(Color.parseColor("#FFFFFF"));
+                break;
+            default:
+                break;
+        }
+
+    }
 }
