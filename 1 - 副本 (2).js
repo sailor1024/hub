71cb57bd47d7bc9727c2186a014926ccc3852d2f
何科盛 <31618447+sailor1024@users.noreply.github.com>
2018-01-01 23:29:59 +0800

    Update 1 - 副本 (2).js

--- 1 - 副本 (2).js
+++ 1 - 副本 (2).js
@@ -1,129 +1,86 @@
-package cn.edu.gdmec.android.testboxuegu.activity;
+package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import cn.edu.gdmec.android.testboxuegu.R;
-import cn.edu.gdmec.android.testboxuegu.utils.MD5Utils;
+import java.util.zip.Deflater;
 
-public class LoginActivity extends AppCompatActivity {
+import cn.edu.gdmec.android.boxuegu.R;
 
-    private TextView tv_main_title;
-    private TextView tv_back;
-    private TextView tv_register;
-    private TextView tv_find_psw;
-    private Button btn_login;
-    private EditText et_user_name;
-    private EditText et_psw;
-    private String userName;
-    private String psw;
-    private String spPsw;
+public class SettingActivity extends AppCompatActivity {
+
+    public static SettingActivity instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_setting);
+        instance = this;
         init();
-
     }
 
     private void init() {
-        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
-        tv_main_title.setText("登录");
-        tv_back = (TextView) findViewById(R.id.tv_back);
-        tv_register = (TextView) findViewById(R.id.tv_register);
-        tv_find_psw = (TextView) findViewById(R.id.tv_find_psw);
-        btn_login = (Button) findViewById(R.id.btn_login);
-        et_user_name = (EditText) findViewById(R.id.et_username);
-        et_psw = (EditText) findViewById(R.id.et_psw);
+
+        TextView tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("设置");
+        TextView tv_back = (TextView) findViewById(R.id.tv_back);
+        RelativeLayout rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.parseColor("#30b4ff"));
+
+        RelativeLayout rl_modify_psw = (RelativeLayout) findViewById(R.id.rl_modify_psw);
+        RelativeLayout rl_security_setting = (RelativeLayout) findViewById(R.id.rl_security_setting);
+        RelativeLayout rl_exit_login = (RelativeLayout) findViewById(R.id.rl_exit_login);
+
         tv_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LoginActivity.this.finish();
-            }
-        });
-        tv_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivityForResult(intent, 1);
+                SettingActivity.this.finish();
             }
         });
 
-        tv_find_psw.setOnClickListener(new View.OnClickListener() {
+        rl_modify_psw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,FindPswActivity.class);
+                //TODO:跳转到修改密码的界面
+                Intent intent = new Intent(SettingActivity.this,ModifyPswActivity.class);
                 startActivity(intent);
-
             }
         });
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        rl_security_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userName = et_user_name.getText().toString().trim();
-                psw = et_psw.getText().toString().trim();
-                String md5Psw = MD5Utils.md5(psw);
-                spPsw=readPsw(userName);
-                if (TextUtils.isEmpty(userName)){
-                    Toast.makeText(LoginActivity.this,"请输入用户名",Toast.LENGTH_SHORT).show();
-                    return;
-                }else if(TextUtils.isEmpty(psw)){
-                    Toast.makeText(LoginActivity.this,"请输入密码",Toast.LENGTH_SHORT).show();
-                    return;
-                }else if(spPsw!=null&&!TextUtils.isEmpty(spPsw) && !md5Psw.equals(spPsw)){
-                    Toast.makeText(LoginActivity.this,"请输入的用户名和密码不一致",Toast.LENGTH_SHORT).show();
-                    return;
-                }else if(md5Psw.equals(spPsw)){
-                    Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
-                    saveLoginStatus(true,userName);
-                    Intent data = new Intent();
-                    data.putExtra("isLogin",true);
-                    setResult(RESULT_OK,data);
-                    LoginActivity.this.finish();
-                    return;
-                }else{
-                    Toast.makeText(LoginActivity.this,"此用户名不存在",Toast.LENGTH_SHORT).show();
-                }
+                //跳转到设置密保的界面
+                Intent intent = new Intent(SettingActivity.this,FindPswActivity.class);
+                intent.putExtra("from","security");
+                startActivity(intent);
             }
         });
 
+        rl_exit_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearLoginStatus();
+                //把退出登录成功后的状态传递到mainActivity中
+                Intent data = new Intent();
+                data.putExtra("isLogin",false);
+                setResult(RESULT_OK,data);
+                SettingActivity.this.finish();
+            }
+        });
     }
 
-    private void saveLoginStatus(boolean status, String userName) {
+    private void clearLoginStatus() {
         SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("isLogin",status);
-        editor.putString("loginUserName",userName);
+        editor.putBoolean("isLogin",false);
+        editor.putString("loginUserName","");
         editor.commit();
-    }
-
-    private String readPsw(String userName) {
-        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
-        return sp.getString(userName,"");
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (data!=null){
-            String userName = data.getStringExtra("userName");
-            if (!TextUtils.isEmpty(userName)){
-                et_user_name.setText(userName);
-                et_user_name.setSelection(userName.length());
-            }
-        }
     }
 }
