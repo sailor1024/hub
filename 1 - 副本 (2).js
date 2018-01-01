428a363b0a942a7502246e9248ac88b28d0ca746
何科盛 <31618447+sailor1024@users.noreply.github.com>
2018-01-01 23:24:38 +0800

    Update 1 - 副本 (2).js

--- 1 - 副本 (2).js
+++ 1 - 副本 (2).js
@@ -1,276 +1,126 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import cn.edu.gdmec.android.boxuegu.R;
-import cn.edu.gdmec.android.boxuegu.view.CourseView;
-import cn.edu.gdmec.android.boxuegu.view.ExercisesView;
-import cn.edu.gdmec.android.boxuegu.view.MyInfoView;
+import cn.edu.gdmec.android.boxuegu.utils.MD5Utils;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private ExercisesView mExercisesView;
-    private CourseView mCourseView;
-    private MyInfoView mMyInfoView;
-    private FrameLayout mBodyLayout;
-    public LinearLayout mBottomLayout;
-    private View mCourseBtn;
-    private View mExercisesBtn;
-    private View mMyInfoBtn;
-    private TextView tv_course;
-    private TextView tv_exercises;
-    private TextView tv_myInfo;
-    private ImageView iv_course;
-    private ImageView iv_exercises;
-    private ImageView iv_myInfo;
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView tv_main_title;
     private TextView tv_back;
-    private TextView tv_mian_title;
-    private RelativeLayout rl_title_bar;
-
+    private TextView tv_register;
+    private TextView tv_find_psw;
+    private Button btn_login;
+    private EditText et_user_name;
+    private EditText et_psw;
+    private String userName;
+    private String psw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         init();
-        initBottomBar();
-        setListener();
-        setInitStatus();
+
     }
 
     private void init() {
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("登录");
         tv_back = (TextView) findViewById(R.id.tv_back);
-        tv_mian_title = (TextView) findViewById(R.id.tv_main_title);
-        tv_mian_title.setText("博学谷课程");
-        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
-        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
-        tv_back.setVisibility(View.GONE);
-        initBodyLayout();
-    }
+        tv_register = (TextView) findViewById(R.id.tv_register);
+        tv_find_psw = (TextView) findViewById(R.id.tv_find_psw);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        et_user_name = (EditText) findViewById(R.id.et_username);
+        et_psw = (EditText) findViewById(R.id.et_psw);
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginActivity.this.finish();
+            }
+        });
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
 
-    private void initBottomBar() {
-        mBottomLayout = (LinearLayout) findViewById(R.id.mian_bottom_bar);
-        mCourseBtn = findViewById(R.id.bottom_bar_course_btn);
-        mExercisesBtn = findViewById(R.id.bottom_bar_exercises_btn);
-        mMyInfoBtn = findViewById(R.id.bottom_bar_myinfo_btn);
-        tv_course = (TextView) findViewById(R.id.bottom_bar_text_course);
-        tv_exercises = (TextView) findViewById(R.id.bottom_bar_text_exercises);
-        tv_myInfo = (TextView) findViewById(R.id.bottom_bar_text_myinfo);
-        iv_course = (ImageView) findViewById(R.id.bottom_bar_image_course);
-        iv_exercises = (ImageView) findViewById(R.id.bottom_bar_image_exercises);
-        iv_myInfo = (ImageView) findViewById(R.id.bottom_bar_image_myinfo);
-    }
+        tv_find_psw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-    private void initBodyLayout() {
-        mBodyLayout = (FrameLayout) findViewById(R.id.main_body);
-    }
+            }
+        });
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.bottom_bar_course_btn:
-                clearBottomImageState();
-                selectDisplayView(0);
-                break;
-            case R.id.bottom_bar_exercises_btn:
-                clearBottomImageState();
-                selectDisplayView(1);
-                break;
-            case R.id.bottom_bar_myinfo_btn:
-                clearBottomImageState();
-                selectDisplayView(2);
-                if (mMyInfoView!=null){
-                   mMyInfoView.setLoginParams(readLoginStatus());
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userName = et_user_name.getText().toString().trim();
+                psw = et_psw.getText().toString().trim();
+                String md5Psw = MD5Utils.md5(psw);
+                String spPsw=readPsw(userName);
+                if (TextUtils.isEmpty(userName)){
+                    Toast.makeText(LoginActivity.this,"请输入用户名",Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(TextUtils.isEmpty(psw)){
+                    Toast.makeText(LoginActivity.this,"请输入密码",Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(!TextUtils.isEmpty(spPsw) && !md5Psw.equals(spPsw)){
+                    Toast.makeText(LoginActivity.this,"请输入的用户名和密码不一致",Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(md5Psw.equals(spPsw)){
+                    Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                    saveLoginStatus(true,userName);
+                    Intent data = new Intent();
+                    data.putExtra("isLogin",true);
+                    setResult(RESULT_OK,data);
+                    LoginActivity.this.finish();
+                    return;
+                }else{
+                    Toast.makeText(LoginActivity.this,"此用户名不存在",Toast.LENGTH_SHORT).show();
                 }
-                break;
-            default:
-                break;
+            }
+        });
 
-        }
     }
 
-    private void setListener() {
-        for (int i = 0; i < mBottomLayout.getChildCount(); i++) {
-            mBottomLayout.getChildAt(i).setOnClickListener(this);
-        }
+    private void saveLoginStatus(boolean status, String userName) {
+        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isLogin",status);
+        editor.putString("loginUserName",userName);
+        editor.commit();
     }
 
-    private void clearBottomImageState() {
-        tv_course.setTextColor(Color.parseColor("#666666"));
-        tv_exercises.setTextColor(Color.parseColor("#666666"));
-        tv_myInfo.setTextColor(Color.parseColor("#666666"));
-        iv_course.setImageResource(R.drawable.main_course_icon);
-        iv_exercises.setImageResource(R.drawable.main_exercises_icon);
-        iv_myInfo.setImageResource(R.drawable.main_my_icon);
-        for (int i = 0; i < mBottomLayout.getChildCount(); i++) {
-            mBottomLayout.getChildAt(i).setSelected(false);
-        }
-    }
+    private String readPsw(String userName) {
+        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
+        return sp.getString(userName,"");
 
-    public void setSelectdStatus(int index) {
-        switch (index) {
-            case 0:
-                mCourseBtn.setSelected(true);
-                iv_course.setImageResource(R.drawable.main_course_icon_selected);
-                tv_course.setTextColor(Color.parseColor("#0097F7"));
-                rl_title_bar.setVisibility(View.VISIBLE);
-                tv_mian_title.setText("博学谷课程");
-                break;
-            case 1:
-                mExercisesBtn.setSelected(true);
-                iv_exercises.setImageResource(R.drawable.main_exercises_icon_selected);
-                tv_course.setTextColor(Color.parseColor("#0097F7"));
-                rl_title_bar.setVisibility(View.VISIBLE);
-                tv_mian_title.setText("博学谷习题");
-                break;
-            case 2:
-                mMyInfoBtn.setSelected(true);
-                iv_myInfo.setImageResource(R.drawable.main_my_icon_selected);
-                tv_course.setTextColor(Color.parseColor("#0097F7"));
-                rl_title_bar.setVisibility(View.GONE);
-        }
-    }
-
-    private void removeAllView() {
-        for (int i = 0; i < mBodyLayout.getChildCount(); i++) {
-            mBodyLayout.getChildAt(i).setVisibility(View.GONE);
-        }
-    }
-
-    private void setInitStatus() {
-        clearBottomImageState();
-        setSelectdStatus(0);
-        createView(0);
-    }
-
-    private void selectDisplayView(int index) {
-        removeAllView();
-        createView(index);
-        setSelectdStatus(index);
-    }
-
-    private void createView(int viewIndex) {
-        switch (viewIndex) {
-            case 0:
-               if (mCourseView==null){
-                    mCourseView=new CourseView(this);
-                    mBodyLayout.addView(mCourseView.getView());
-                }else {
-                   mCourseView.getView();
-                }
-                mCourseView.showView();
-                break;
-            case 1:
-                if (mExercisesView == null) {
-                    mExercisesView = new ExercisesView(this);
-                    mBodyLayout.addView(mExercisesView.getView());
-                } else {
-                    mExercisesView.getView();
-                }
-                mExercisesView.showView();
-                break;
-            case 2:
-                if (mMyInfoView==null){
-                    mMyInfoView=new MyInfoView(this);
-                    mBodyLayout.addView(mMyInfoView.getView());
-                }else {
-                    mMyInfoView.getView();
-               }
-                mMyInfoView.showView();
-                break;
-        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data != null) {
-            boolean isLogin = data.getBooleanExtra("isLogin", false);
-            if (isLogin) {
-                clearBottomImageState();
-                selectDisplayView(0);
-            }
-            if (mMyInfoView!=null){
-                mMyInfoView.setLoginParams(isLogin);
+        if (data!=null){
+            String userName = data.getStringExtra("userName");
+            if (!TextUtils.isEmpty(userName)){
+                et_user_name.setText(userName);
+                et_user_name.setSelection(userName.length());
             }
         }
     }
-
-
-
-
-
-
-    protected long exitTime;
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK
-                && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if ((System.currentTimeMillis() - exitTime) > 2000) {
-                Toast.makeText(MainActivity.this, "再按一次退出博学谷",
-                        Toast.LENGTH_SHORT).show();
-                exitTime = System.currentTimeMillis();
-            } else {
-                MainActivity.this.finish();
-                if (readLoginStatus()) {
-                    clearLoginStatus();
-                }
-                System.exit(0);
-            }
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-
-    }
-
-    private boolean readLoginStatus() {
-        SharedPreferences sp = getSharedPreferences("loginInfo",
-                Context.MODE_PRIVATE);
-        boolean isLogin = sp.getBoolean("isLogin", false);
-        return isLogin;
-    }
-
-    private void clearLoginStatus() {
-        SharedPreferences sp = getSharedPreferences("loginInfo",
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("isLogin", false);
-        editor.putString("loginUserName","");
-        editor.commit();
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
