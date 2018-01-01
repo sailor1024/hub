40bbbb7dcb04072c42d4798491cde9b92bf88140
何科盛 <31618447+sailor1024@users.noreply.github.com>
2018-01-01 23:35:06 +0800

    Update 1 - 副本 (2).js

--- 1 - 副本 (2).js
+++ 1 - 副本 (2).js
@@ -1,85 +1,114 @@
-package cn.edu.gdmec.android.testboxuegu.activity;
+package cn.edu.gdmec.android.boxuegu.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import cn.edu.gdmec.android.testboxuegu.R;
+import cn.edu.gdmec.android.boxuegu.R;
+import cn.edu.gdmec.android.boxuegu.activity.LoginActivity;
+import cn.edu.gdmec.android.boxuegu.activity.PlayHistoryActivity;
+import cn.edu.gdmec.android.boxuegu.activity.SettingActivity;
+import cn.edu.gdmec.android.boxuegu.activity.UserInfoActivity;
+import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
 
-public class SettingActivity extends AppCompatActivity {
-    public static  SettingActivity instance = null;
-    private TextView tv_main_title;
-    private TextView tv_back;
-    private RelativeLayout rl_title_bar;
-    private RelativeLayout rl_modify_psw;
-    private RelativeLayout rl_security_setting;
-    private RelativeLayout rl_exit_login;
+/**
+ * Created by student on 17/12/28.
+ */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        instance = this;
-        init();
+public class MyInfoView {
+    public ImageView iv_head_icon;
+    private LinearLayout ll_head;
+    private RelativeLayout rl_course_history,rl_setting;
+    private TextView tv_user_name;
+    private Activity mContext;
+    private LayoutInflater mInflater;
+    private View mCurrentView;
+    public MyInfoView(Activity context){
+        mContext= context;
+        mInflater= LayoutInflater.from(mContext);
+    }
+    private void createView(){
+        initView();
     }
 
-    private void init() {
-        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
-        tv_main_title.setText("设置");
-        tv_back = (TextView) findViewById(R.id.tv_back);
-        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
-        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
-        rl_modify_psw = (RelativeLayout) findViewById(R.id.rl_modify_psw);
-        rl_security_setting = (RelativeLayout) findViewById(R.id.rl_security_setting);
-        rl_exit_login = (RelativeLayout) findViewById(R.id.rl_exit_login);
-        tv_back.setOnClickListener(new View.OnClickListener() {
+    private void initView() {
+        mCurrentView = mInflater.inflate(R.layout.main_view_myinfo,null);
+        ll_head = (LinearLayout)mCurrentView.findViewById(R.id.ll_head);
+        iv_head_icon = (ImageView) mCurrentView.findViewById(R.id.iv_head_icon);
+        rl_course_history = (RelativeLayout) mCurrentView.findViewById(R.id.rl_course_history);
+        rl_setting = (RelativeLayout) mCurrentView.findViewById(R.id.rl_setting);
+        tv_user_name = (TextView) mCurrentView.findViewById(R.id.tv_user_name);
+        mCurrentView.setVisibility(View.VISIBLE);
+        setLoginParams(readLoginStatus());
+        ll_head.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SettingActivity.this.finish();
+                if (readLoginStatus()){
+                    Intent intent = new Intent(mContext, UserInfoActivity.class);
+                    mContext.startActivity(intent);
+                }else{
+                    Intent intent = new Intent(mContext, LoginActivity.class);
+                    mContext.startActivityForResult(intent,1);
+                }
             }
         });
-        rl_modify_psw.setOnClickListener(new View.OnClickListener() {
+        rl_course_history.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SettingActivity.this,ModifyPswActivity.class);
-                startActivity(intent);
+                if (readLoginStatus()){
+                    Intent intent = new Intent(mContext, PlayHistoryActivity.class);
+                    mContext.startActivity(intent);
+                }else{
+                    Toast.makeText(mContext,"您还为登录，请先登录",Toast.LENGTH_SHORT).show();
+                }
             }
         });
-        rl_security_setting.setOnClickListener(new View.OnClickListener() {
+        rl_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SettingActivity.this,FindPswActivity.class);
-                intent.putExtra("from","security");
-                startActivity(intent);
+                if (readLoginStatus()){
+                    Intent intent = new Intent(mContext, SettingActivity.class);
+                    mContext.startActivityForResult(intent,1);
+                }else{
+                    Toast.makeText(mContext,"您还为登录，请先登录",Toast.LENGTH_SHORT).show();
+                }
             }
         });
-        rl_exit_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingActivity.this,"退出登录成功",Toast.LENGTH_SHORT).show();
-                clearLoginStatus();
-                Intent data = new Intent();
-                data.putExtra("isLogin",false);
-                setResult(RESULT_OK,data);
-                SettingActivity.this.finish();
-            }
-        });
+
     }
 
-    private void clearLoginStatus(){
-        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("isLogin",false);
-        editor.putString("loginUserName","");
-        editor.commit();
+    public void setLoginParams(boolean isLogin){
+        if (isLogin){
+            tv_user_name.setText(AnalysisUtils.readLoginUserName(mContext));
+        }
+        else{
+            tv_user_name.setText("点击登录");
+        }
     }
+    public View getView(){
+        if (mCurrentView==null){
+            createView();
+        }return mCurrentView;
+    }
+    public void showView(){
+        if (mCurrentView==null){
+            createView();
+        }
+        mCurrentView.setVisibility(View.VISIBLE);
+    }
+
+    private boolean readLoginStatus(){
+        SharedPreferences sp = mContext.getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        boolean isLogin = sp.getBoolean("isLogin",false);
+        return isLogin;
+    }
+
 }
